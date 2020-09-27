9a412b139199c8f8fd0c23054e43a26d0d2acccc
yugi0922 <yugi-0922@outlook.jp>
2020-09-27 19:47:50 +0900

    デザインパターン：iterator

---
+++ designPattern/src/iterator/Main.java
@@ -0,0 +1,19 @@
+package iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		BookShelf bookShelf = new BookShelf(4);
+		bookShelf.appendBook(new Book("1"));
+		bookShelf.appendBook(new Book("2"));
+		bookShelf.appendBook(new Book("3"));
+		bookShelf.appendBook(new Book("4"));
+		Iterator it = bookShelf.iterator();
+		while(it.hasNext()){
+			Book book = (Book)it.next();
+			System.out.println(book.getName());
+		}
+	}
+
+}
