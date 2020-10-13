1cb8190cd0c1af1733a72a05befbc6bc0bebdeb2
yugi0922 <yugi-0922@outlook.jp>
2020-10-14 07:19:48 +0900

    デザインパターン：protoType

---
+++ designPattern/src/protoType/UnderlinePen.java
@@ -0,0 +1,31 @@
+package protoType;
+
+import protoType.flameWork.Product;
+
+public class UnderlinePen implements Product {
+	private char ulChar;
+	public UnderlinePen(char ulChar){
+		this.ulChar = ulChar;
+	}
+	@Override
+	public void use(String s) {
+		int length = s.getBytes().length;
+		System.out.println("\"" + s + "\"");
+		System.out.print(" ");
+		for(int i = 0; i < length; i++){
+			System.out.print(ulChar);
+		}
+		System.out.println("");
+	}
+
+	@Override
+	public Product createClone() {
+		Product p = null;
+		try{
+			p = (Product)clone();
+		} catch (CloneNotSupportedException e){
+			e.printStackTrace();
+		}
+		return p;
+	}
+}
