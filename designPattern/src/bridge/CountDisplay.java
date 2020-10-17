a229a6032188af6da1e3ecfaa4f330e79a59f5b1
yugi0922 <yugi-0922@outlook.jp>
2020-10-18 08:10:20 +0900

    デザインパターン：bridge

---
+++ designPattern/src/bridge/CountDisplay.java
@@ -0,0 +1,17 @@
+package bridge;
+
+public class CountDisplay extends Display {
+
+	public CountDisplay(DisplayImpl impl) {
+		super(impl);
+	}
+
+	public void multiDisplay(int items){
+		open();
+		for(int i = 0; i < items; i++){
+			print();
+		}
+		close();
+	}
+
+}
